2f9721270cced57b378e77f8d9333e8a42947dba
BoFu001 <bofu001@gmail.com>
2026-06-09 18:24:45 +0100

    Disable table copy and fullscreen buttons in Streamdown, keep download only

--- src/components/ReportPanel.jsx
+++ src/components/ReportPanel.jsx
@@ -4,7 +4,7 @@ import 'streamdown/styles.css'
 function ReportPanel({ report, isStreaming }) {
   return (
     <div className="rounded-none px-4 py-3">
-      <Streamdown isAnimating={isStreaming}>
+      <Streamdown isAnimating={isStreaming} controls={{ table: { copy: false, download: true, fullscreen: false } }}>
         {report}
       </Streamdown>
     </div>
